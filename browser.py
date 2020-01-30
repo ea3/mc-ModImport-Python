5314b8843bd6cb6d8e0cf52319bf2559367d6416
Emilio Araos <ea3dev@gmail.com>
2020-01-29 22:40:12 -0700

    webbrowser example

--- browser.py
+++ browser.py
@@ -1,5 +1,18 @@
 import webbrowser
+#
+# webbrowser.open("https://www.nba.com", new=2)
+#
+# help(webbrowser)
 
-webbrowser.open("https://www.nba.com")
 
-help(webbrowser)
+# for i in range(1,10):
+#     print(1,2,3,4,5,6,7,8,9, sep="", end=' ')
+
+chrome = webbrowser.get(using='chrome')
+chrome.open_new("https://www.nba.com")
+
+safari = webbrowser.get(using='safari')
+safari.open_new("https://www.nfl.com")
+
+
+
