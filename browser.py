7ac2202d1468f1772b8f7f2f47ba583fdacb0550
Emilio Araos <ea3dev@gmail.com>
2020-01-29 22:14:41 -0700

    webbrowser module

---
+++ browser.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+webbrowser.open("https://www.nba.com")
+
+help(webbrowser)
--- exampl.py
+++ exampl.py
@@ -1,1 +1,7 @@
-print(dir())
+import shelve
+
+help(shelve)
+# for obj in dir(shelve.Shelf):
+#     if obj[0] != '_':
+#         print(obj)
+#
